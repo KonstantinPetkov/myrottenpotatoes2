bed268e0a8c8f107a35b415e5e8c860730a05aef
Konstantin <konstantin.petkow@gmail.com>
2017-12-30 11:20:23 +0000

    sorting & filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,25 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:title => :asc}, 'hilite'
+    when 'release_date'
+      ordering,@date_header = {:release_date => :asc}, 'hilite'
+    end
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+     
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+     
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
   
   def show
@@ -9,6 +28,7 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.html.haml by default
   end
   
+  # add to the movies_controller.rb
   def new
     @movie = Movie.new
     # default: render 'new' template
@@ -29,17 +49,19 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find params[:id]
   end
- 
+   
   def update
     @movie = Movie.find params[:id]
     params.require(:movie)
     permitted = params[:movie].permit(:title,:rating,:release_date)
+    
     if @movie.update_attributes(permitted)
       flash[:notice] = "#{@movie.title} was successfully updated."
       redirect_to movie_path(@movie)
     else
       render 'edit' # note, 'edit' template can access @movie's field values!
     end
+    
   end
   
   def destroy
@@ -48,5 +70,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
+  
 end
